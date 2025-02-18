92f26c4581610f94731eea2dd0a45ef4f9ad2ad2
pucheranni <gugugupavo@gmail.com>
2025-02-18 09:39:51 -0300

    Sum and diff add

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,15 @@
 public class Calculadora {
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+    public static int diff (int a, int b){
+        return a-b;
+    }
 
+    public static void main(String[] args) {
+        System.out.printf("%d",sum(10,1));
+        System.out.printf("\n%d", diff(1,10));
+    }
     //something
 
 }
